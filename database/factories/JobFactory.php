5cae673bd014d493a05cac49d6ac19dba0348574
alnahian2003 <a.alnahian2003@gmail.com>
2022-08-24 01:53:48 +0600

    Job Seeder Created

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'tags' => 'laravel, development, remote',
+            'company' => $this->faker->company(),
+            'location' => $this->faker->city(),
+            'email' => $this->faker->email(),
+            'website' => $this->faker->url(),
+            'description' => $this->faker->paragraph(5),
+        ];
+    }
+}
