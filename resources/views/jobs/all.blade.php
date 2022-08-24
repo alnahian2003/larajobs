78b09fb6cdae0cc0759c6005d9a7ce6eac2aa07b
alnahian2003 <a.alnahian2003@gmail.com>
2022-08-24 22:26:43 +0600

    looping choice changed from foreach to forelse

--- resources/views/jobs/all.blade.php
+++ resources/views/jobs/all.blade.php
@@ -5,17 +5,14 @@
 @include('partials._search_form')
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-    @foreach ($jobs as $job)
-    {{-- Job Card --}}
+    @forelse ($jobs as $job)
+        {{-- Job Card --}}
     <x-job-card :job="$job"  />
-
-    @empty($jobs)
-    <h3>No Jobs Available</h3>    
-    @endempty
-
-    @endforeach
+    @empty
+    <h3 class="my-6 text-3xl font-semibold text-gray-400">No Jobs Available</h3>    
+    @endforelse
 </div>
-<div class="my-8 mx-14">
+<div class="my-8 mx-6">
     {{ $jobs->links() }}
 </div>
 </x-layout>
