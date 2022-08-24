e544bc13c2aeb5d96aa460139f95af2777c7eb10
alnahian2003 <a.alnahian2003@gmail.com>
2022-08-24 12:58:07 +0600

    Job Controller Created

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -6,7 +6,7 @@
 
 <ul class="flex">
     @foreach ($tags as $tag)
-    <li class="bg-slate-800 text-white rounded-xl px-3 py-1 mr-2">
+    <li class="bg-slate-800 text-white rounded-full px-3 py-1 mr-2">
         <a href="?tag={{$tag}}">{{$tag}}</a>
     </li>
     @endforeach
--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -9,7 +9,7 @@ class="relative h-72 bg-laravel flex flex-col justify-center align-center text-c
 
 <div class="z-10">
     <h1 class="text-6xl font-bold uppercase text-white">
-        Lara<span class="text-black">Gigs</span>
+        Lara<span class="text-black">Jobs</span>
     </h1>
     <p class="text-2xl text-gray-200 font-bold my-4">
         Find or post jobs & projects
